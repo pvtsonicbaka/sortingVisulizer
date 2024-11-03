ea206fe807039bce9040c5d679cdcadc8855fdc6
pvtsonicbaka <ashiyanap1680@gmail.com>
2024-11-03 12:25:24 +0530

    added a new function to do rectangle

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello world")
+from tkinter import *
+from random import *
+from tkinter import ttk
+root=Tk()
+root.title("Sorting some alogorithms")
+root.geometry("1500x1000")
+root.config(background="black")
+
+algo=StringVar()
+
+AlgortithmMenu = Label(root,text="Algortihm",font="Arial 15 italic bold" ,bg="blue",
+width="10"
+)
+AlgortithmMenu.place(x="10",y="10")
+
+algo_menu= ttk.Combobox(root,font="bold" ,textvariable=algo,values=['Bubble sort','Insetion sort','Merge Sort'],
+                        width="10"
+                        )
+algo_menu.place(x="130",y="11")
+
+root.mainloop()
